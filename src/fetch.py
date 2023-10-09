e3f8a79983d58437d2b30646496ac93df658c3df
CoolJosh0221 <joshluffy99@gmail.com>
2023-10-09 13:39:32 +0800

    first api

---
+++ src/fetch.py
@@ -0,0 +1,31 @@
+import requests
+
+
+async def get_response(url: str):
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return {"error": "Failed to fetch weather data"}
+
+
+async def fetch_daily_weather_forecast(lat: float, lon: float):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto"
+
+    response = await get_response(url)
+    return response
+
+
+async def fetch_hourly_weather_forecast(lat: float, lon: float):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m,rain,showers,snowfall,uv_index&timezone=auto&forecast_days=1"
+
+    response = await get_response(url)
+    return response
+
+
+async def fetch_air_quality_forecast(lat: float, lon: float):
+    url = f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={lat}&longitude={lon}&hourly=pm10,pm2_5"
+
+    response = await get_response(url)
+    return response
---
+++ src/server.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI, Query
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from icecream import ic
+import json
+
+from fetch import (
+    fetch_daily_weather_forecast,
+    fetch_hourly_weather_forecast,
+    fetch_air_quality_forecast,
+)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/get_weather")
+async def get_weather(
+    lat: float = Query(..., title="Latitude", description="Latitude coordinate"),
+    lon: float = Query(..., title="Longitude", description="Longitude coordinate"),
+):
+    # JSON data
+    daily_report = await fetch_daily_weather_forecast(lat, lon)
+    hourly_report = await fetch_hourly_weather_forecast(lat, lon)
+    air_quality_report = await fetch_air_quality_forecast(lat, lon)
+    # Accessing daily weather data
+    hourly_weather = hourly_report["hourly"]
+    daily_weather = daily_report['daily']
+    max_temperatures = daily_weather['temperature_2m_max']
+    precipitation_probabilities = daily_weather['precipitation_probability_max']
+    uv_index = hourly_report['hourly']['uv_index']
+
+    notify_umbrella = map(
+        lambda x: x > 30,
+        precipitation_probabilities,
+    )  # to notify user to bring umbrella
+    notify_jacket = map(
+        lambda x: x < 20,
+        max_temperatures,
+    )  # notify user to bring jacket
+    notify_sunscreen = map(
+        lambda x: x >= 5,
+        uv_index,
+    )
+
+    additional_data_daily = {
+        "notify_umbrella": list(notify_umbrella),
+        "notify_jacket": list(notify_jacket),
+    }
+
+    additional_data_hourly = {
+        "notify_sunscreen": list(notify_sunscreen),
+    }
+    hourly_weather.update(air_quality_report["hourly"])
+    hourly_weather.update(additional_data_hourly)
+    daily_weather.update(additional_data_daily)
+    daily_report["hourly"] = hourly_report["hourly"]
+    daily_report["hourly_units"] = hourly_report["hourly_units"]
+
+    ic(daily_report)
+
+    return json.dumps(daily_report)
