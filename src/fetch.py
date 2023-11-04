84615db9b5df0d0be69cac17482d45a6cfda63f1
CoolJosh0221 <joshluffy99@gmail.com>
2023-11-04 19:21:23 +0800

    Change to unixtimestamp

--- src/fetch.py
+++ src/fetch.py
@@ -11,21 +11,21 @@ async def get_response(url: str):
 
 
 async def fetch_daily_weather_forecast(lat: float, lon: float):
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto"
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto&timeformat=unixtime"
 
     response = await get_response(url)
     return response
 
 
 async def fetch_hourly_weather_forecast(lat: float, lon: float):
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m,rain,showers,snowfall,uv_index&timezone=auto&forecast_days=1"
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m,rain,showers,snowfall,uv_index&timezone=auto&forecast_days=1&timeformat=unixtime"
 
     response = await get_response(url)
     return response
 
 
 async def fetch_air_quality_forecast(lat: float, lon: float):
-    url = f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={lat}&longitude={lon}&hourly=pm10,pm2_5"
+    url = f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={lat}&longitude={lon}&hourly=pm10,pm2_5&timeformat=unixtime"
 
     response = await get_response(url)
     return response
