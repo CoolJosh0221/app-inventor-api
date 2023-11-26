ce6eae1864c2a97d8b72b27734d541fad2546d6e
CoolJosh0221 <joshluffy99@gmail.com>
2023-11-26 23:28:17 +0800

    audio fix4

--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -1,9 +1,10 @@
-import uuid
-import random
-import string
+import inspect
+import os
 from gtts import gTTS
 
 
 async def generate(message: str, lang: str):
+    root_dir = os.getcwd()
+
     tts = gTTS(message, lang=lang)
-    tts.save("audio/output.mp3")
+    tts.save(f"{root_dir}/audio/output.mp3")
