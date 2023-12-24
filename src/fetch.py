3cf27e179b0fb6692d5a1671c26224c99feefea6
CoolJosh0221 <joshluffy99@gmail.com>
2023-12-24 17:34:06 +0800

    refactor + items checklist

--- src/fetch.py
+++ src/fetch.py
@@ -1,31 +1,69 @@
-import requests
+import aiohttp
+from urllib.parse import urlencode
+
+BASE_URL = "https://api.open-meteo.com/v1"
 
 
 async def get_response(url: str):
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        return {"error": "Failed to fetch weather data"}
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    return data
+                else:
+                    return {"error": "Failed to fetch weather data"}
+    except aiohttp.ClientError as e:
+        return {"error": f"Failed to fetch weather data: {str(e)}"}
 
 
 async def fetch_daily_weather_forecast(lat: float, lon: float):
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=auto&timeformat=unixtime"
+    if not (-90 <= lat <= 90):
+        raise ValueError("Latitude must be between -90 and 90")
+    if not (-180 <= lon <= 180):
+        raise ValueError("Longitude must be between -180 and 180")
+
+    params = {
+        'latitude': lat,
+        'longitude': lon,
+        'daily': 'temperature_2m_max,temperature_2m_min,precipitation_probability_max',
+        'timezone': 'auto',
+        'timeformat': 'unixtime',
+    }
+    url = f"{BASE_URL}/forecast?{urlencode(params)}"
 
     response = await get_response(url)
     return response
 
 
 async def fetch_hourly_weather_forecast(lat: float, lon: float):
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=cloud_cover,temperature_2m,rain,showers,snowfall,uv_index&timezone=auto&forecast_days=1&timeformat=unixtime"
+    if not (-90 <= lat <= 90):
+        raise ValueError("Latitude must be between -90 and 90")
+    if not (-180 <= lon <= 180):
+        raise ValueError("Longitude must be between -180 and 180")
+
+    params = {
+        'latitude': lat,
+        'longitude': lon,
+        'hourly': 'cloud_cover,temperature_2m,rain,showers,snowfall,uv_index',
+        'timezone': 'auto',
+        'forecast_days': 1,
+        'timeformat': 'unixtime',
+    }
+    url = f"{BASE_URL}/forecast?{urlencode(params)}"
 
     response = await get_response(url)
     return response
 
 
 async def fetch_air_quality_forecast(lat: float, lon: float):
-    url = f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={lat}&longitude={lon}&hourly=pm10,pm2_5&timeformat=unixtime"
+    if not (-90 <= lat <= 90):
+        raise ValueError("Latitude must be between -90 and 90")
+    if not (-180 <= lon <= 180):
+        raise ValueError("Longitude must be between -180 and 180")
+
+    params = {'latitude': lat, 'longitude': lon, 'hourly': 'pm10,pm2_5', 'timeformat': 'unixtime'}
+    url = f"https://air-quality-api.open-meteo.com/v1/air-quality?{urlencode(params)}"
 
     response = await get_response(url)
     return response
--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -1,10 +1,23 @@
-import inspect
 import os
 from gtts import gTTS
 
 
 async def generate(message: str, lang: str, tld: str):
-    root_dir = os.getcwd()
+    """
+    Generate speech from the given message and save it as an audio file.
 
-    tts = gTTS(message, lang=lang, tld=tld)
-    tts.save(f"{root_dir}/audio/output.mp3")
+    Args:
+        message (str): The message to convert to speech.
+        lang (str): The language of the message.
+        tld (str): The top-level domain for the gTTS service.
+    """
+    if message:
+        root_dir = os.getcwd()
+
+        tts = gTTS(message, lang=lang, tld=tld)
+        try:
+            tts.save(f"{root_dir}/audio/output.mp3")
+        except Exception as e:
+            print(f"Error occurred during tts.save: {e}")
+    else:
+        print("Message is empty. No speech generated.")
